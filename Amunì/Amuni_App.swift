746bcca9cdb8da9caca8edcef08bff1b08395ca1
Giovanniballariano <Giovanniballariano@gmail.com>
2025-02-13 10:17:44 +0100

    Initial Commit

---
+++ Amunì/Amuni_App.swift
@@ -0,0 +1,17 @@
+//
+//  Amuni_App.swift
+//  Amunì
+//
+//  Created by Giovanni Ballariano on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Amuni_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
